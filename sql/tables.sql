300e2e4b0f78c71e63d7c03067494a2d3dfc09b7
Kim Brugger <kim.brugger@easih.ac.uk>
2014-07-16 09:44:50 +0100

    added a gender column to the sample table

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,54 +1,19 @@
-drop database VCFdb;
-create database VCFdb;
-use VCFdb;
-
-
-CREATE TABLE plate (
-  pid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name                VARCHAR(20) NOT NULL ,
-
-  KEY name_idx (name)
-);
-
+drop database depths_exome_5bp;
+create database depths_exome_5bp;
+use depths_exome_5bp;
 
 
 CREATE TABLE sample (
 
   sid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name                VARCHAR(40) NOT NULL ,
 
-  KEY name_idx (name)
-);
-
-
-
-CREATE TABLE sample_sequence (
-
-  ssid                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  sid                 INT NOT NULL,
-  pid                 INT NOT NULL,
   name                VARCHAR(80) NOT NULL,
   total_reads	      INT,
   mapped_reads	      INT,
   duplicate_reads     INT,
-  mean_isize	      float,
+  mean_isize	      float
+  gender              EMUM ('F', 'M', 'U' ) DEFAULT 'U',
 
-  KEY sid_idx  (sid),
-  KEY pid_idx  (pid)
-);
-
-
-CREATE TABLE variant (
-
-  vid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  chr                 VARCHAR(8) NOT NULL ,
-  pos		      INT NOT NULL,
-  ref                 VARCHAR(100) NOT NULL ,
-  alt                 VARCHAR(100) NOT NULL ,
-  comment	      VARCHAR(200),
-  annotation	      VARCHAR(500),
-
-  KEY pos_idx  (chr, pos, ref, alt)
 );
 
 
@@ -58,43 +23,40 @@ CREATE TABLE region (
   chr                 VARCHAR(8) NOT NULL ,
   start               INT NOT NULL,
   end                 INT NOT NULL,
-  name                VARCHAR(100) NOT NULL ,
-  reference	      VARCHAR(100) NOT NULL,
 
-  KEY pos_idx  (chr, start, end),
-  KEY ref_idx (reference),
-  KEY name_idx (name)
+  KEY pos_idx  (chr, start, end)
 );
+
+CREATE TABLE transcript (
+ rid INT NOT NULL,
+
+ exon_name varchar(300),
+ refseq    varchar(300),
+ CCDS      varchar(300),
+
+ KEY ref_idx (refseq),
+ KEY name_idx (exon_name),
+
+ KEY rid_idx (rid)
+);
+ 
 
 CREATE TABLE coverage (
 
-#  cid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  ssid                INT NOT NULL,
+  sid                INT NOT NULL,
   rid                 INT NOT NULL,
 
   min		      FLOAT,  
   mean		      FLOAT,  
   max		      FLOAT,  
-  lows		      VARCHAR(300),
   missing	      VARCHAR(300),
+  1to5		      VARCHAR(300),
+  6to9		      VARCHAR(300),
+  10to19	      VARCHAR(300),
 
-  KEY ssid_idx  (ssid),
+  KEY sid_idx  (sid),
   KEY rid_idx  (rid)
 );
 
-
-
-CREATE TABLE sample_variant (
-
-  ssid                INT NOT NULL,
-  vid                 INT NOT NULL,
-
-  depth		      INT,  
-  AAF		      FLOAT,  
-  quality	      FLOAT,  
-
-  KEY ssid_idx  (ssid),
-  KEY vid_idx  (vid)
-);
 
 
